99cdb184a4f05847b90491251f4607deedb6d286
Ahmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2021-12-04 23:58:01 +0500

    npm packages

--- js/signIn.js
+++ js/signIn.js
@@ -1,4 +1,30 @@
 let ms = require("mssql");
 let connectionStr={
-    serverName:"AHMED\SQLEXPRESS"
+    serverName:'AHMED\SQLEXPRESS',
+    database:'SampleDataLab',
+    user:"",
+    password:""
 }
+
+function getData() {
+    let con = new ms.connect(connectionStr);
+    let req = new ms.Request(con);
+    con.connect(function(err){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        req.query("Select * from Customer",function(err,data){
+            if (err) {
+                console.log(err);
+                return;
+            }
+            else{
+                console.log(data);
+            }
+            con.close();
+        })
+    })
+}
+
+getData();
