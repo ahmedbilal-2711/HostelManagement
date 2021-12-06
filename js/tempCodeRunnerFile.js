dbe98bb5578c67a7bbfd4a9879c5fe6476d9f3ad
Ahmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2021-12-07 00:52:51 +0500

    backend tested

--- js/tempCodeRunnerFile.js
+++ js/tempCodeRunnerFile.js
@@ -1,35 +1,10 @@
-let ms = require("mssql");
-let connectionStr={
-    serverName:'AHMED\\SQLEXPRESS',
-    database:'SampleDataLab',
-    username:'Ahmed\\Ahmed Bilal Siddiqui'
-}
-
-function getData() {
-    let con = new ms.connect(connectionStr,function(err) {
-        if (err) {
-            console.log(err);
-        }
-        else{
-            let req = new ms.Request(con);
-            con.connect(function(err){
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                req.query("Select * from customer",function(err,data){
-                    if (err) {
-                        console.log(err);
-                        return;
-                    }
-                    else{
-                        console.log(data);
-                    }
-                    con.close();
-                })
-            })
-        }
-    });
-}
-
-getData();
+var config = {
+    "user": 'Ahmed\\Ahmed Bilal Siddiqui',
+    "server": 'AHMED\\SQLEXPRESS',
+    "database": 'database_name',
+    "port": 1433, // make sure to change port
+    "dialect": "mssql",
+    "dialectOptions": {
+        "instanceName": "SQLEXPRESS"
+    }
+};
---
+++ signIn.js
@@ -0,0 +1,103 @@
+/* const { request } = require('http');
+const sql = require('mssql');
+const config = {
+    server:'AHMED\\SQLEXPRESS',
+    username:'HostelDb',
+    password:'hmdb',
+    database:'SampleDataLab',
+    port:1434
+}
+
+function getData() {
+    // var conn = new sql.Connection(config);
+    sql.connect(config,function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        else{
+            var req = new sql.Request();
+            req.query('SELECT * from Employee',function(err,data){
+                if (err) {
+                    console.log(err);
+                }
+                else{
+                    console.log(data);
+                }
+                conn.close();
+            })
+    
+        }
+    });
+}
+
+getData(); */
+
+/* const { request } = require('http');
+const sql = require('mssql');
+const config = {
+    server:'AHMED\\SQLEXPRESS',
+    username:'HostelDb',
+    password:'hmdb',
+    database:'SampleDataLab',
+    port:1434
+}
+
+function getData() {
+    // var conn = new sql.Connection(config);
+    sql.connect(config,function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        else{
+            var req = new sql.Request();
+            req.query('SELECT * from Employee',function(err,data){
+                if (err) {
+                    console.log(err);
+                }
+                else{
+                    console.log(data);
+                }
+                conn.close();
+            })
+    
+        }
+    });
+}
+
+getData(); */
+
+let express = require("express");
+let app = express();
+app.use(express.urlencoded({extented:true}));
+
+app.use(express.static(__dirname));
+
+app.post('/',function (req,res) {
+    console.log(req.body);
+    console.log(req.body.em);
+    console.log(req.body.pass);
+    res.sendFile('../html/bioForm.html')
+});
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/html/index.html");
+
+});
+
+app.listen(5540, function(){
+    console.log("kdjfkjfkd");
+});
+
+
+
+
+
+
+
+
+
+
+
+
