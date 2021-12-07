5103746fd598204dec376ae8af9a008c5846a7e6
Ahmed Bilal Siddiqui <ahmedbilal6880@gmail.com>
2021-12-07 11:31:19 +0500

    Backend

--- signIn.js
+++ signIn.js
@@ -1,7 +1,7 @@
 /* const { request } = require('http');
 const sql = require('mssql');
 const config = {
-    server:'AHMED\\SQLEXPRESS',
+    servername:'AHMED\\SQLEXPRESS',
     username:'HostelDb',
     password:'hmdb',
     database:'SampleDataLab',
@@ -33,42 +33,48 @@ function getData() {
 
 getData(); */
 
-/* const { request } = require('http');
-const sql = require('mssql');
-const config = {
-    server:'AHMED\\SQLEXPRESS',
-    username:'HostelDb',
-    password:'hmdb',
-    database:'SampleDataLab',
-    port:1434
-}
+const { default: knex } = require("knex");
+var sql = require("mssql");
+var config = {
+  user: "HostelDb",
+  password: "hmdb",
+  server: "AHMED\\SQLEXPRESS",
+  database: "SampleDataLab",
+  port: 1434,
+  options: {
+    encrypt: false,
+    useUTC: true,
+  },
 
-function getData() {
-    // var conn = new sql.Connection(config);
-    sql.connect(config,function (err) {
+  pool: {
+  /*   max: 100,
+    min: 0,
+    idleTimeoutMillis: 3600000,
+    connectionTimeout: 3600000,
+    requestTimeout: 3600000, */
+  },
+};
+var database = new sql.ConnectionPool(config);
+
+database
+  .connect()
+  .then(function () {
+    console.log("connected");
+    database.query("SELECT * FROM Employee",function (err,data) {
         if (err) {
-            console.log(err);
-            return;
+          console.log(err);
         }
         else{
-            var req = new sql.Request();
-            req.query('SELECT * from Employee',function(err,data){
-                if (err) {
-                    console.log(err);
-                }
-                else{
-                    console.log(data);
-                }
-                conn.close();
-            })
-    
+          console.log(data);
         }
-    });
-}
+    })
+    database.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-getData(); */
-
-let express = require("express");
+/* let express = require("express");
 let app = express();
 app.use(express.urlencoded({extented:true}));
 
@@ -88,7 +94,7 @@ app.get('/',function(req,res){
 
 app.listen(5540, function(){
     console.log("kdjfkjfkd");
-});
+}); */
 
 
 
