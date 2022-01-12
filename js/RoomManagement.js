a26f7252f24c573cad848da68d36342a9523fcf9
Muneeb_Official <muneeb.official.686332@hotmail.com>
2022-01-12 11:35:21 +0500

    biofrom deletd

--- js/RoomManagement.js
+++ js/RoomManagement.js
@@ -1,13 +1,24 @@
-const flat=document.querySelectorAll(".heading");
+const flat1=document.getElementById("flat1");
+const flat2=document.getElementById("flat2");
+const flat3=document.getElementById("flat3");
+const flat4=document.getElementById("flat4");
+const flat5=document.getElementById("flat5");
+const flat6=document.getElementById("flat6");
+const flat7=document.getElementById("flat7");
+const flat8=document.getElementById("flat8");
+const flat9=document.getElementById("flat9");
+const flat10=document.getElementById("flat10");
+const flat11=document.getElementById("flat11");
 
 function transition(){
+  flat1.style.transitionDuration="3s";
 
-    prompt("hello");
 }
 
-flat.forEach(function() {
-    transition();
-  });
+
+// flat.forEach(function() {
+//     transition();
+//   });
 
 
-flat.addEventListener("click",transition);
+flat1.addEventListener("hover",transition);
